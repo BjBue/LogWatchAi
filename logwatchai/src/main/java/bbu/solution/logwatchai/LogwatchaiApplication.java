0105e252ddc01b50a6feafe8a3e763c54c52b6dc
bjoern-buechau <bjoern.buechau@smartfuturecampus.onmicrosoft.com>
2025-11-17 17:16:48 +0100

    extract

---
+++ logwatchai/src/main/java/bbu/solution/logwatchai/LogwatchaiApplication.java
@@ -0,0 +1,13 @@
+package bbu.solution.logwatchai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogwatchaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogwatchaiApplication.class, args);
+	}
+
+}
---
+++ logwatchai/src/test/java/bbu/solution/logwatchai/LogwatchaiApplicationTests.java
@@ -0,0 +1,13 @@
+package bbu.solution.logwatchai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogwatchaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
