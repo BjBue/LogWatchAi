f856ad1eb67b8b92cc6e08f9af2f4d69eef716c3
bjoern-buechau <bjoern.buechau@smartfuturecampus.onmicrosoft.com>
2025-11-18 10:24:29 +0100

    running app and db and first endpoint

--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 Ein lokales Java-Tool überwacht Logdateien, analysiert
 neue Einträge in Echtzeit mithilfe von KI und leitet
 automatisch passende Maßnahmen oder Warnungen
+
+
+Projekt neu erstellen:
+ind der konsole im verzecihniss mit dem pom.xml:
+$ .\mvnw.cmd clean package
+$ docker-compose down   //evtl. mit -v (löscht volumes)
+$ docker-compose build
+$ docker-compose up -d
+
---
+++ logwatchai/src/main/java/bbu/solution/logwatchai/infrastructure/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package bbu.solution.logwatchai.infrastructure.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("health").permitAll()   // public
+                        .anyRequest().authenticated()
+                )
+                .csrf(AbstractHttpConfigurer::disable)  // no csrf
+                .formLogin(Customizer.withDefaults()); //standard login-form
+        return http.build();
+    }
+
+}
