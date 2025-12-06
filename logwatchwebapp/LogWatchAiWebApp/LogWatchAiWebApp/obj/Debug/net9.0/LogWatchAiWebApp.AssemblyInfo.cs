7472827ea83bf97106b3007a6cd8bcc55e8b1ccd
bjoern-buechau <bjoern.buechau@smartfuturecampus.onmicrosoft.com>
2025-12-06 10:30:16 +0100

    nicer dicer filter actions

--- logwatchwebapp/LogWatchAiWebApp/LogWatchAiWebApp/obj/Debug/net9.0/LogWatchAiWebApp.AssemblyInfo.cs
+++ logwatchwebapp/LogWatchAiWebApp/LogWatchAiWebApp/obj/Debug/net9.0/LogWatchAiWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LogWatchAiWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6d64835bd30ad8971c2b34a86aec38a53b18f36")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+503e3c4d849dbbbdc28838197f32dd961463f8df")]
 [assembly: System.Reflection.AssemblyProductAttribute("LogWatchAiWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LogWatchAiWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
